8f8a72071b8567de063c855d2e0ebfcf2db21001
Russell Selph <dev@selph.net>
2021-03-07 06:00:27 -0700

    Fetch library or playlist

--- main.go
+++ main.go
@@ -9,22 +9,81 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path"
+	"strings"
 )
 
 var (
-	srv            = "http://192.168.3.12:32400"
+	srv            string
 	selectPlaylist string
+	selectLibrary  string
 )
 
 func main() {
-	flag.StringVar(&selectPlaylist, "playlist", "Movie Night",
-		"playlist to get images from")
+	flag.StringVar(&selectPlaylist, "playlist", "", "playlist to get images from")
+	flag.StringVar(&selectLibrary, "library", "", "library to get images from")
+	flag.StringVar(&srv, "plex", "", "URL of plex server")
 	flag.Parse()
-	if flag.NArg() > 0 {
-		srv = flag.Arg(0)
+
+	if srv == "" {
+		srv = os.Getenv("PLEX")
 	}
 
+	if srv == "" {
+		fmt.Println("must specify the plex server")
+		os.Exit(1)
+	}
+
+	srv = strings.TrimRight(srv, "/")
+
+	if selectPlaylist != "" {
+		fetchPlaylist()
+	}
+
+	if selectLibrary != "" {
+		fetchLibrary()
+	}
+}
+
+func plexGet(key string, objOut interface{}) {
+	resp, err := http.Get(srv + key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	err = xml.Unmarshal(bodyBytes, objOut)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+
+func fetchPoster(thumb string) {
+	resp, err := http.Get(srv + thumb)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	fileName := strings.ReplaceAll(thumb, "/", "_") + ".jpg"
+	fileName = strings.TrimLeft(fileName, "_")
+	fmt.Println(fileName)
+	f, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fetchPlaylist() {
 	var allPlaylists struct {
 		Playlists []struct {
 			Key   string `xml:"key,attr"`
@@ -51,42 +110,37 @@ func main() {
 	plexGet(key, &onePlaylist)
 
 	for _, video := range onePlaylist.Videos {
-		resp, err := http.Get(srv + video.Thumb)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fileName := path.Base(video.Thumb) + ".jpg"
-		fmt.Println(fileName)
-		f, err := os.Create(fileName)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = io.Copy(f, resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		f.Close()
-		resp.Body.Close()
+		fetchPoster(video.Thumb)
 	}
-
 }
 
-func plexGet(key string, objOut interface{}) {
-	resp, err := http.Get(srv + key)
-	if err != nil {
-		log.Fatal(err)
+func fetchLibrary() {
+	var allLibraries struct {
+		Libraries []struct {
+			Key   string `xml:"key,attr"`
+			Title string `xml:"title,attr"`
+		} `xml:"Directory"`
 	}
-	defer resp.Body.Close()
+	plexGet("/library/sections", &allLibraries)
 
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-
-	err = xml.Unmarshal(bodyBytes, objOut)
-	if err != nil {
-		log.Fatal(err)
+	var key string
+	for _, l := range allLibraries.Libraries {
+		if l.Title == selectLibrary {
+			key = l.Key
+		}
+	}
+	if key == "" {
+		log.Fatal("no such library")
 	}
 
-	return
+	var oneLibrary struct {
+		Videos []struct {
+			Thumb string `xml:"thumb,attr"`
+		} `xml:"Video"`
+	}
+	plexGet("/library/sections/"+key+"/all", &oneLibrary)
+
+	for _, video := range oneLibrary.Videos {
+		fetchPoster(video.Thumb)
+	}
 }
